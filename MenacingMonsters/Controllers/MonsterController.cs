b23bffa36b089c581e2e73334acc85819185f9a5
Andy Collins <andy.collins@relode.com>
2019-10-29 13:06:33 -0500

    initial commit

---
+++ MenacingMonsters/Controllers/MonsterController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MenacingMonsters.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace MenacingMonsters.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonsterController : ControllerBase
+    {
+        private string _connectionString;
+
+        public MonsterController(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        // GET: api/Monster
+        [HttpGet]
+        public IEnumerable<Monster> Get()
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT id, name, eyeCount, catchPhrase FROM monster";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Monster> monsters = new List<Monster>();
+                    while (reader.Read())
+                    {
+                        Monster newMonster = new Monster()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("id")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                            EyeCount = reader.GetInt32(reader.GetOrdinal("eyeCount")),
+                            CatchPhrase = reader.GetString(reader.GetOrdinal("catchPhrase"))
+                        };
+                        monsters.Add(newMonster);
+                    }
+
+                    reader.Close();
+
+                    return monsters;
+                }
+            }
+        }
+
+        // GET: api/Monster/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id, name, eyeCount, catchPhrase 
+                                          FROM monster
+                                         WHERE id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Monster aMonster = null;
+                    if (reader.Read())
+                    {
+                        aMonster = new Monster()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("id")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                            EyeCount = reader.GetInt32(reader.GetOrdinal("eyeCount")),
+                            CatchPhrase = reader.GetString(reader.GetOrdinal("catchPhrase"))
+                        };
+                    }
+
+                    reader.Close();
+
+                    if (aMonster == null)
+                    {
+                        return NotFound();
+                    }
+
+                    return Ok(aMonster);
+                }
+            }
+         }
+
+        // POST: api/Monster
+        [HttpPost]
+        public void Post([FromBody] Monster newMonster)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Monster (name, eyecount, catchphrase)
+                                        VALUES (@name, @eyecount, @catchphrase)";
+                    cmd.Parameters.Add(new SqlParameter("@name", newMonster.Name));
+                    cmd.Parameters.Add(new SqlParameter("@eyecount", newMonster.EyeCount));
+                    cmd.Parameters.Add(new SqlParameter("@catchphrase", newMonster.CatchPhrase));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // PUT: api/Monster/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MonstersDB.sql
@@ -0,0 +1,18 @@
+﻿Create Table Monster (
+	Id integer Identity primary key not null,
+	Name varchar(50) not null,
+	EyeCount integer not null,
+	CatchPhrase varchar(255)
+);
+
+
+insert into Monster (name, eyecount, catchphrase) 
+values ('Wolfman', 2, 'owwhouwhwu');
+
+insert into Monster (name, eyecount, catchphrase) 
+values ('Giant Spider', 8, 'what-chu-talkin-bout');
+
+insert into Monster (name, eyecount, catchphrase) 
+values ('cyclops', 1, 'I cannot see depth');
+
+
